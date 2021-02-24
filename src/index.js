e868e8ad1c6df51e908605af20c9e1c1c0e9c2d4
Pius Onwurah <onwurahpius41@gmail.com>
2021-02-24 20:55:54 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
+
+ReactDom.render(<Counter />, document.getElementById('root'))
